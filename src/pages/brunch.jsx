159c2641e60aa38612bed6602817b908b436c375
imsourceboom <ecmascript.developer@gmail.com>
2019-05-30 17:24:57 +0900

    testt

--- src/pages/brunch.jsx
+++ src/pages/brunch.jsx
@@ -8,36 +8,7 @@ const SecondPage = () => (
   <Layout headerUse>
     <SEO title="Brunch" />
     <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>
-    <h1>Brunch</h1>d<p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>Welcome to Brunch</p>
-    <p>dWelcome to Brunch</p>
+    <b>맛있는 요리</b>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
