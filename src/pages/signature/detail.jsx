40e170b37011ea7265b26a542993d927b6a4a07b
imsourceboom <ecmascript.developer@gmail.com>
2019-06-01 00:28:24 +0900

    props error modify please

--- src/pages/signature/detail.jsx
+++ src/pages/signature/detail.jsx
@@ -20,22 +20,71 @@ const Detail = ({ location }) => {
   // const activeIndex = String()
   return (
     <Layout notMain>
-      {/* {console.log(props.location)} */}
       <SEO title="signature" />
-      <Swiper {...params} activeSlideKey={location.state.slideKey}>
-        <Item className="swiper-slide" key="0">
-          <Image filename="signature/almond.jpg" />
-        </Item>
-        <Item className="swiper-slide" key="1">
-          <Image filename="signature/nutella.jpg" />
-        </Item>
-        <Item className="swiper-slide" key="2">
-          <Image filename="signature/red-blue.jpg" />
-        </Item>
-        <Item className="swiper-slide" key="3">
-          <Image filename="signature/refresh.jpg" />
-        </Item>
-      </Swiper>
+      {location.state.slideKey === "0" && (
+        <Swiper {...params} activeSlideKey="0">
+          <Item className="swiper-slide" key="0">
+            <Image filename="signature/almond.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="1">
+            <Image filename="signature/nutella.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="2">
+            <Image filename="signature/red-blue.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="3">
+            <Image filename="signature/refresh.jpg" />
+          </Item>
+        </Swiper>
+      )}
+      {location.state.slideKey === "1" && (
+        <Swiper {...params} activeSlideKey="1">
+          <Item className="swiper-slide" key="0">
+            <Image filename="signature/almond.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="1">
+            <Image filename="signature/nutella.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="2">
+            <Image filename="signature/red-blue.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="3">
+            <Image filename="signature/refresh.jpg" />
+          </Item>
+        </Swiper>
+      )}
+      {location.state.slideKey === "2" && (
+        <Swiper {...params} activeSlideKey="2">
+          <Item className="swiper-slide" key="0">
+            <Image filename="signature/almond.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="1">
+            <Image filename="signature/nutella.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="2">
+            <Image filename="signature/red-blue.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="3">
+            <Image filename="signature/refresh.jpg" />
+          </Item>
+        </Swiper>
+      )}
+      {location.state.slideKey === "3" && (
+        <Swiper {...params} activeSlideKey="3">
+          <Item className="swiper-slide" key="0">
+            <Image filename="signature/almond.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="1">
+            <Image filename="signature/nutella.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="2">
+            <Image filename="signature/red-blue.jpg" />
+          </Item>
+          <Item className="swiper-slide" key="3">
+            <Image filename="signature/refresh.jpg" />
+          </Item>
+        </Swiper>
+      )}
     </Layout>
   )
 }
