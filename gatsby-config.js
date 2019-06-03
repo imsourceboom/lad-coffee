e9994325c3f7fd802fd80af763dbf0bdcc4c1a8f
imsourceboom <ecmascript.developer@gmail.com>
2019-06-03 11:33:10 +0900

    offline pulgin add

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
         icon: `src/images/lad.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
